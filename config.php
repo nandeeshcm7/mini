76f13d38c0ddd1e4a3f97f2a4a66e6975b100c06
nandeeshcm7 <36222483+nandeeshcm7@users.noreply.github.com>
2018-03-14 22:20:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 
 <?php
-$connect=mysql_connect("localhost","root");
+$connect=mysql_connect("den1.mysql2.gear.host","attendancedb");
 if(!$connect)
 {
 	echo "Error".mysql_error();
 	}
 	
 	
-	$db=mysql_select_db("attendance_db");
+	$db=mysql_select_db("attendancedb");
 	if(!$db)
 	{
 		echo "Error".mysql_error();
